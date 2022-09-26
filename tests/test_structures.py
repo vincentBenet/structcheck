6a21cf0acfcba133b24bdb0f5c70f19effcd2cea
vincentBenet <vincent.benet@outlook.fr>
2022-09-26 16:03:49 +0200

    poetry add

--- tests/test_structures.py
+++ tests/test_structures.py
@@ -5,7 +5,7 @@ import inspect
 import unittest
 
 current_dir = os.path.dirname(__file__)
-source_dir = os.path.join(current_dir, "..", "source")
+source_dir = os.path.join(current_dir, "..", "structcheck")
 sys.path.insert(0, source_dir)
 
 import structcheck
@@ -14,15 +14,18 @@ class TestStructures(unittest.TestCase):
     def test_succes(self):
         path_data_succes = os.path.join(current_dir, "data_succes")
         for folder in os.listdir(path_data_succes):
-            print(f"\n{inspect.stack()[0][3]}: {folder}")
             path_folder = os.path.join(path_data_succes, folder)
+            print(f"\n{'_' * 115}\n{'_' * 115}\n{inspect.stack()[0][3]}: {folder} - < {path_folder} >\n{'_' * 115}\n{'_' * 115}")
             args = structcheck.check_args({
                 "root": path_folder
             })
-            _, reports, logs = structcheck.main(
+            txt, reports, logs = structcheck.main(
                 args["root"], args["conf"], args["report"], args["data"],
                 display=False
             )
+            
+            print(txt)
+            
             self.assertEqual(len(reports), 0)  # Check no error
             
             # Check files are generated
@@ -32,15 +35,18 @@ class TestStructures(unittest.TestCase):
     def test_fail(self):
         path_data_succes = os.path.join(current_dir, "data_fail")
         for folder in os.listdir(path_data_succes):
-            print(f"\n{inspect.stack()[0][3]}: {folder}")
             path_folder = os.path.join(path_data_succes, folder)
+            print(f"\n{'_' * 115}\n{'_' * 115}\n{inspect.stack()[0][3]}: {folder} - < {path_folder} >\n{'_' * 115}\n{'_' * 115}")
             args = structcheck.check_args({
                 "root": path_folder
             })
-            _, reports, logs = structcheck.main(
+            txt, reports, logs = structcheck.main(
                 args["root"], args["conf"], args["report"], args["data"],
                 display=False
             )
+            
+            print(txt)
+            
             self.assertEqual(len(reports), 1)  # Check exactly one error
             
             # Check files are generated
