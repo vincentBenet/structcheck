203d45b7abfe3175ed63f4223430a5f7571b243a
vincentBenet <vincent.benet@outlook.fr>
2022-12-13 01:17:58 +0100

    formatting

--- structcheck/to_graphviz.py
+++ structcheck/to_graphviz.py
@@ -1,13 +1,16 @@
 """
 Nodes attributes: https://graphviz.org/docs/nodes/
 Edges attributes: https://graphviz.org/docs/edges/
+
+Code non maintenable car trop chiant à expliquer...
+C'est du récursif, toussa mais en plus c'est mal fait (il faudrait le reprendre)
 """
 import copy
 import os
 import re
 import graphviz
 
-os.environ["PATH"] += os.pathsep + r"C:\Program Files\Graphviz\bin"
+os.environ["PATH"] += os.pathsep + r"C:\Program Files\Graphviz\bin"  # Add graphiz to path for windows OS
 
 
 def data_create(dico, config, nodes=None, links=None):
@@ -36,6 +39,7 @@ def data_create(dico, config, nodes=None, links=None):
                         attributes_node = config.get("nodes", {}).get("Files", {})
                         attributes_edge = config.get("edges", {}).get("Files", {})
 
+                # Mise sous UID des nodes dans les listes
                 node = el
                 i = 1
                 el += "_" + str(i)
@@ -43,9 +47,12 @@ def data_create(dico, config, nodes=None, links=None):
                     el = el[:-len(str(i))]
                     i += 1
                     el += str(i)
-                    if i == 2:
-                        print()
-                nodes[el] = attributes_node | copy.deepcopy(config["names"].get(node, {})) | copy.deepcopy(nodes.get(node, {}))
+
+                nodes[el] = (
+                    attributes_node |
+                    copy.deepcopy(config["names"].get(node, {})) |
+                    copy.deepcopy(nodes.get(node, {}))
+                )
 
                 nodes[el]["label"] = nodes[el]["regex"]
                 if nodes[el]["label"].startswith("^"):
@@ -79,22 +86,52 @@ def data_create(dico, config, nodes=None, links=None):
                 ])
 
         elif isinstance(dico[elem], dict):
+
+            # Mise sous UID des nodes dans les dictionnaires
+            el = elem
+            node = el
+            i = 1
+            el += "_" + str(i)
+            while el in nodes:
+                el = el[:-len(str(i))]
+                i += 1
+                el += str(i)
+
             attributes_node = config.get("nodes", {}).get("tree", {})
             attributes_edge = config.get("edges", {}).get("tree", {})
             links.append([
                 source,
-                elem,
+                el,
                 attributes_edge
             ])
 
-            nodes[elem] = attributes_node | config["names"].get(elem, {})
-            nodes[elem]["label"] = nodes[elem]["regex"]
-            if nodes[elem]["label"].startswith("^"):
-                nodes[elem]["label"] = nodes[elem]["label"][1:]
-            if nodes[elem]["label"].endswith("$"):
-                nodes[elem]["label"] = nodes[elem]["label"][:-1]
-            if nodes[elem]["label"] != elem:
-                nodes[elem]["xlabel"] = elem
+            nodes[el] = attributes_node | config["names"].get(node, {})
+            nodes[el]["label"] = nodes[el]["regex"]
+            if nodes[el]["label"].startswith("^"):
+                nodes[el]["label"] = nodes[el]["label"][1:]
+            if nodes[el]["label"].endswith("$"):
+                nodes[el]["label"] = nodes[el]["label"][:-1]
+            if nodes[el]["label"] != node:
+                nodes[el]["xlabel"] = node
+
+            nodes[el]["tooltip"] = f"{el}: {nodes[el].get('description', 'No description')}\n"
+            matches = re.findall("{.*?}", nodes[el]["regex"])
+            if matches:
+                nodes[el]["tooltip"] += "\n\n"
+                regex_example = nodes[el].get("regex", el)
+                for match in matches:
+                    key = match.replace("{", "").replace("}", "")
+                    if key not in config.get("variables", {}):
+                        continue
+                    regex_key = config.get("variables", {}).get(key)
+                    nodes[el]["tooltip"] += match + f" : ({regex_key.get('regex', '')}) = " + regex_key.get(
+                        "description", "")
+                    if "example" in regex_key:
+                        nodes[el]["tooltip"] += f" -> {regex_key['example']}"
+                        regex_example = regex_example.replace(match, regex_key["example"])
+                    nodes[el]["tooltip"] += '\n'
+                if len(matches):
+                    nodes[el]["tooltip"] += f"\nExample: {regex_example}"
             nodes, links = data_create(dico[elem], config, nodes=nodes, links=links)
         
         else:
@@ -110,37 +147,11 @@ def main(config):
         config,
         nodes={"root": config["nodes"]["root"] | {"label": repr(config['paths']['root'])}}
     )
-
     f = graphviz.Digraph('Graph', filename=config['paths']['graph'], format="svg")
     f.attr(**config.get("graph", {}))
     f.attr('node', **config.get("nodes", {}).get("default", {}))
     f.attr('edge', **config.get("edges", {}).get("default", {}))
     for node in nodes:
-        # nodes[node]["tooltip"] = f"{node}"
-        # if nodes[node].get('description', ""):
-        #     nodes[node]["tooltip"] += f" : {nodes[node]['description']}"
-        # matches = re.findall("{.*?}", nodes[node].get("regex", node))
-        # if matches:
-        #     nodes[node]["tooltip"] += "\n\n"
-        # regex_example = nodes[node].get("regex", node)
-        # for match in matches:
-        #     key = match.replace("{", "").replace("}", "")
-        #     if key not in config.get("variables", {}):
-        #         continue
-        #     regex_key = config.get("variables", {}).get(key)
-        #     nodes[node]["tooltip"] += match + f" : ({regex_key.get('regex', '')}) = " + regex_key.get("description", "")
-        #     if "example" in regex_key:
-        #         nodes[node]["tooltip"] += f" -> {regex_key['example']}"
-        #         regex_example = regex_example.replace(match, regex_key["example"])
-        #     nodes[node]["tooltip"] += '\n'
-        # if len(matches):
-        #     nodes[node]["tooltip"] += f"\nExample: {regex_example}"
-        # if "label" not in nodes[node]:
-        #     nodes[node]["label"] = nodes[node]["regex"]
-        # if nodes[node]["label"].startswith("^"):
-        #     nodes[node]["label"] = nodes[node]["label"][1:]
-        # if nodes[node]["label"].endswith("$"):
-        #     nodes[node]["label"] = nodes[node]["label"][:-1]
         f.node(node, **nodes[node])
     for link in links:
         f.edge(link[0], link[1], **link[2])
