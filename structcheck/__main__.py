b7a1f818d105b0ebeae8e21d4eebb8926caa7447
vincentBenet <vincent.benet@outlook.fr>
2022-12-13 16:04:46 +0100

    new version

--- structcheck/__main__.py
+++ structcheck/__main__.py
@@ -4,11 +4,14 @@ import pkg_resources
 
 
 if __name__ == "__main__":
+    version = pkg_resources.get_distribution('structcheck').version
+    directory = os.getcwd()
 
-    print(f"structckeck version = {pkg_resources.get_distribution('structcheck').version}")
+    print(f"structckeck version = {version}")
+    print(f"Scanning directory = {directory}")
 
     raise SystemExit(
         structcheck.scan(
-            ["-p", os.getcwd()]
+            ["-p", directory]
         )
     )
